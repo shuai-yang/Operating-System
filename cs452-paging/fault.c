7ad2e684a6e1bc9eb02de904cda05d7c7292b895
shuai-yang <shuaiyang@u.boisestate.edu>
2022-10-05 16:41:37 -0600

    initial framework

--- cs452-paging/fault.c
+++ cs452-paging/fault.c
@@ -42,14 +42,60 @@ int is_entire_table_free(unsigned long table){
  * 2 == permissions error
  * return 0 if handled successful; otherwise return -1.
  * */
+uintptr_t get_free_page();
 
 int infiniti_do_page_fault(struct infiniti_vm_area_struct *infiniti_vma, uintptr_t fault_addr, u32 error_code) {
+	unsigned long cr3;	
+	unsigned kernel_addr; //uintptr_t kernel_addr
+	unsigned long pml4, pdp, pd, pt;
+    unsigned long *pml4_entry, *pdp_entry, *pd_entry, *pt_entry; 
+
+	//fault_addr should within the reserved memory region and application should have permission to access fault address
+	if(is_valid_address(infiniti_vma, fault_addr)==0 || error_code == SEGV_ACCERR)
+		return -1;
+
+	//get the content of the cr3 register
+	cr3 = get_cr3();
+	//
+	pml4 = __va(cr3 & 0xffffffffff000); 
+	//base address + offset
+	pml4_entry = (unsigned long*)pml4 + (((fault_addr >> 39) & 0xfff) << 3);
+    //
+    if(!(pml4_entry & 0x1)){
+		get_free_page();
+		pml4_entry = pml4_entry | 0x1;
+		pml4_entry = pml4_entry | 0x2;
+		pml4_entry = pml4_entry | 0x4
+	}
+ 
+    //if
+
 	printk("Page fault!\n");
-    return -1;
+    return -1; //return 0 if a page fault is handled successfully
+}
+
+uintptr_t get_free_page(){
+	uintptr_t kernel_addr = 0;
+	kernel_addr = (uintptr_t)get_zeroed_page(GFP_KERNEL);
+	if (!kernel_addr) {
+		printk(KERN_INFO "failed to allocate one page\n");
+		return -ENOMEM;
+	}	
+	return kernel_addr;
 }
 
 /* this function takes a user VA and free its PA as well as its kernel va. */
 void infiniti_free_pa(uintptr_t user_addr){
+	// is_entire_table_free(unsigned long table);
+	unsigned long page_table;
+	if(is_entire_table_free(page_table)){
+		do something;
+	}else{
+		do something;
+	}
+
+	free_page(kernel_addr);
+	
 	return;
 }
 
