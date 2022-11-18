e72110ef6704030df8a39de18c75d932d45607c4
shuai-yang <shuaiyang@u.boisestate.edu>
2022-11-17 17:55:22 -0700

    make sure no newline at end of script fill to avoid generating whitespaces

--- cs452-device-driver-2/run.sh
+++ cs452-device-driver-2/run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 sudo echo 'D' > /proc/lincoln/cmd
-echo "Hi, the keyboard is disable for 7 seconds"
-sleep 7 
+echo "Hi, the keyboard is disable for 3 seconds"
+sleep 3
 sudo echo 'E' > /proc/lincoln/cmd
 sudo echo 'R' > /proc/lincoln/cmd
-
 sudo echo 'E' > /proc/lincoln/cmd
